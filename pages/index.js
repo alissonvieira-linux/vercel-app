bbec5c6c719d4f9b3c69e22ea61a1d7d0ddf70a5
Alisson Vieira <alissonvieira.github@gmail.com>
2020-11-10 01:34:35 -0300

    Request for github API and rendering the users info an a component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
+import { useEffect, useState } from 'react';
+
 function HomePage() {
-  return <h1>Olá mozão lindo do meu coração!</h1>
+  const [userInfo, setUserInfo] = useState({});
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/alissonvieira-linux')
+      .then(response => response.json())
+      .then(data => {
+        setUserInfo(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <p>Name: <strong>{userInfo.name}</strong></p>
+      <p>Username: <strong>{userInfo.login}</strong></p>
+      <p>Bio: <strong>{userInfo.bio}</strong></p>
+      <p>City: <strong>{userInfo.location}</strong></p>
+    </div>
+  );
 }
 
 export default HomePage;
